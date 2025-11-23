1eeec6de5c956bf1abd1ef711edf917df7e75539
barenxing <richard.hu@me.com>
2025-11-23 18:30:46 -0500

    Renamed dashboard.lua to snacks.lua as dashboard is part of the snacks plugin

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,8 @@
+-- Check out this YouTube video on folke snasks plugin
+-- https://youtu.be/NG7P_fPeuA8?si=eAJVJOHQ-guQ3zT1
+return {
+  {
+    "folke/snacks.nvim",
+    opts = { dashboard = { enabled = false } },
+  },
+}
