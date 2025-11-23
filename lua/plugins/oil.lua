593809ace9b044ca8d47358633e1ab0f3dd0c53e
barenxing <richard.hu@me.com>
2025-11-23 18:12:28 -0500

    Enabled oil plugin

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/oil.nvim",
+
+  opts = {
+    columns = {
+      "icon",
+      "mtime",
+      "size",
+      -- "permissions",
+    },
+  },
+
+  vim.keymap.set("n", "<leader>-", "<cmd>Oil --float<CR>"),
+
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
