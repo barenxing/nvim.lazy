370c9566343f5407b7394e9ee228018008a128ad
barenxing <richard.hu@me.com>
2026-05-03 11:30:39 -0400

    removed an extra line in the mltemplate

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -57,7 +57,6 @@ ls.add_snippets("python", {
       "#",
       "",
       "",
-      "#",
       "# Split the dataset into training and testing sets",
       "(X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.2, random_state=0)",
       "",
